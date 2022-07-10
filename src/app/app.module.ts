33f4b252d232ca76e9f38cb66a9391f053814f70
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-10 07:55:22 +0200

    Add bulma test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { DemoComponent } from './demo/demo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DemoComponent
+    DemoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
